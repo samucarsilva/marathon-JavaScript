2d2b81cc56780917bb70d72b52c14023ca256888
samucarsilva <samucarsilva007@gmail.com>
2026-02-25 10:48:57 -0300

    feat: Concatenando e Interpolando Strings

---
+++ Proj-001/scripts.js
@@ -0,0 +1,51 @@
+
+// Desafio 01
+// Calculando o Lucro
+
+const valorBoloPote = 12.0;
+const custoProd = 5.50;
+let = quantidadeVendida = 30;
+
+const lucroUnidade = valorBoloPote - custoProd;
+const lucroTotal = valorBoloPote * quantidadeVendida;
+
+console.log("Calculando o Lucro");
+console.log("Lucro Unidade: R$ " + lucroUnidade);
+console.log("Lucro Total: R$ " + lucroTotal + "\n");
+
+
+// Desafio 02
+// Calculando Desconto
+
+const valorBolo = 150.0;
+let descontoBolo = 10;
+
+const valorDesconto = (valorBolo * descontoBolo) / 100;
+const valorFinalBolo = valorBolo - valorDesconto;
+
+console.log("Calculando o Desconto");
+console.log("Valor Bolo: R$ " + valorBolo);
+console.log("Valor Desconto: R$ " + valorDesconto);
+console.log("Valor Final: R$ " + valorFinalBolo + "\n");
+
+
+// Desafio 03
+// Calculando Comissão
+
+const valorVestido = 800.0;
+const percentualVenda = 15;
+const valorComissao = (valorVestido * percentualVenda) / 100;
+console.log("Calculando a Comissão");
+console.log(`Valor Comissão: R$ ${valorComissao} \n`);
+
+
+// Desafio 04
+// Faturamento Mensal
+ 
+  const valorSessao = 120.0;
+  let sessoesMensais = 40;
+  const faturamentoMensal = valorSessao * sessoesMensais;
+
+  console.log("Calculando o Faturamento");
+  console.log("Faturamento Mensal: R$ " + faturamentoMensal + "\n");
+ 
